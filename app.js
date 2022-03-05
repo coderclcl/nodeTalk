646667702874a64240a460b55ff5c5c15015338a
coderclcl <gattenmaster@gmail.com>
2022-03-05 22:02:37 +0900

    sequelize 초안

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ const session = require('express-session');
 const nunjucks = require('nunjucks');
 const dotenv = require('dotenv');
 
+
 dotenv.config();
 const pageRouter = require('./routes/page');
+const { sequelize } = require('./models');
 
 const app = express();
 app.set('port', process.env.PORT || 7777);
@@ -16,6 +18,13 @@ nunjucks.configure('views', {
     express: app,
     watch: true,
 });
+sequelize.sync({ force: false })
+    .then(() => {
+        console.log('데이터베이스 연결 성공');
+    })
+    .catch((err) => {
+        console.error(err);
+    });
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
--- models/user.js
+++ models/user.js
@@ -37,5 +37,33 @@ module.exports = class User extends Sequelize.Model {
         });
     }
 
-    static associate(db) {}
+    static associate(db) {
+
+        // User 테이블과 UserInfo 테이블의 관계 
+        db.User.hasOne(db.UserInfo, {
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE'
+        });
+        
+        // User 테이블과 User 테이블의 관계 (Friend 중간 테이블 만들기)
+        db.User.belongsToMany(db.User, {
+            foreignKey: 'followingId',
+            as: 'Followers',
+            through: 'Friend' // 중간 테이블 이름 
+        });
+
+        db.User.belongsToMany(db.User, {
+            foreignKey: 'followerId',
+            as: 'Followings',
+            through: 'Friend' // 중간 테이블 이름 
+        });
+
+        // User 테이블과 Room 테이블의 관계 (Member 중간 테이블 만들기)
+        db.User.belongsToMany(db.Room, {
+            through: 'Member',
+            foreignKey: 'uid',
+        });
+
+
+    }
 };
